6b1d0c44c9100b65d1d4ddd0fee82792dc2ac28c
Nitin Nandan <nitinkrs1980@gmail.com>
2025-07-18 22:20:11 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./sections/navbar";
 
 const App = () => {
-  return <div className="text-3xl font-bold underline text-red-500">App</div>;
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      {/* <Hero /> */}
+      {/* <About /> */}
+      {/* <Projects /> */}
+      {/* <Experiences /> */}
+      {/* <Testimonial /> */}
+      {/* <Contact /> */}
+      {/* <Footer/> */}
+    </div>
+  );
 };
 
 export default App;
