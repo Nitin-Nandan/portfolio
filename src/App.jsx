2c84421ded3dad95cec81342bd6ef7242552a933
Nitin Nandan <nitinkrs1980@gmail.com>
2025-07-18 23:28:08 +0530

    Added Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
-      {/* <Hero /> */}
+      <Hero />
       {/* <About /> */}
       {/* <Projects /> */}
       {/* <Experiences /> */}
