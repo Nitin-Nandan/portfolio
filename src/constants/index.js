edb376dc42ae423cc18e3e77c25b99efba539d8d
Nitin Nandan <nitinkrs1980@gmail.com>
2025-07-19 19:10:19 +0530

    Added Experiences

--- src/constants/index.js
+++ src/constants/index.js
@@ -95,36 +95,15 @@ export const mySocials = [
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "Data Science Intern",
+    job: "CodeClause",
+    date: "2025",
     contents: [
-      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
-    ],
-  },
-  {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
-    contents: [
-      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
-    ],
-  },
-  {
-    title: "Freelance Developer",
-    job: "Self-Employed",
-    date: "2025-Present",
-    contents: [
-      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+      "Engineered a deep learning application for real-time image recognition with a streamlined interface, leveraging CNNs and Flask for seamless user interaction.",
+      "Delivered a high-performance recommendation engine using collaborative filtering and intelligent caching, supporting advanced filters, confidence scoring, and dynamic content presentation.",
+      "Developed an interactive dashboard to visualize user preferences and recommendation trends, with export capabilities for CSV and PDF formats.",
+      "Applied K-Means clustering to segment customers based on purchasing patterns, enabling targeted business strategies through unsupervised learning.",
+      "Designed and deployed a time series forecasting model to predict retail demand, incorporating seasonality and trend analysis for accurate future insights.",
     ],
   },
 ];
